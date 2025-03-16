198eaa5ff6f587b6c2b7d4e0ba54b999eeb66405
Edgardo Guzman Jr <eegyolk@gmail.com>
2025-03-16 00:53:36 -0600

    Implemented TodoProvider and TodoTable

--- 02-hooks/src/App.jsx
+++ 02-hooks/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import { ThemeProvider } from './context/ThemeContext'
 import ThemeToggle from './components/ThemeToggle'
+import { TodoProvider } from './context/TodoContext'
+import TodoTable from './components/TodoTable'
 
 function App() {
   return (
     <ThemeProvider>
-      <h1>Implementing Hooks in Todo Apps</h1>
+      <h1>Implementing Hooks in Todo App</h1>
       <ThemeToggle />
+      <TodoProvider>
+        <TodoTable />
+      </TodoProvider>
     </ThemeProvider>
   )
 }
